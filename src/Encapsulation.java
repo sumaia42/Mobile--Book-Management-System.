7d6c4d5e7a402ae148f48b78e86830ef55f91681
Sumaia Akter <121504983+sumaia42@users.noreply.github.com>
2023-05-11 12:39:23 +0600

    Update Encapsulation.java

--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Encapsulation extends Polymorphism {
 
     String name;
@@ -22,6 +25,22 @@ public class Encapsulation extends Polymorphism {
 
     public void setEmail(String email) {
         this.email = email;
+
+        Pattern pattern3 = Pattern.compile("((?=.*[a-z])([0-9])(?=.*[@.]).{10,30})");// must have to given small latter and @ and . or any number......
+        Matcher matcher3 = pattern3.matcher(email);
+
+
+        if (matcher3.find()) {
+
+
+            System.out.println("");
+        }
+
+        else{
+            System.out.println("Invalid email!");
+
+
+        }
     }
 
     public String getAddress() {
@@ -40,12 +59,33 @@ public class Encapsulation extends Polymorphism {
         this.peraddress = peraddress;
     }
 
+
+
+
     public String getMoboile() {
         return moboile;
     }
 
     public void setMoboile(String moboile) {
         this.moboile = moboile;
+
+
+
+        Pattern pattern2 = Pattern.compile("((?=.*[0-9]).{11,11})");//must have to give 11 degite number....
+        Matcher matcher2 = pattern2.matcher(moboile);
+
+
+        if (matcher2.find()) {
+
+
+            System.out.println("");
+        }
+
+        else{
+            System.out.println("Invalid Mobile Number!");
+
+
+        }
     }
 
     public String getDate() {
